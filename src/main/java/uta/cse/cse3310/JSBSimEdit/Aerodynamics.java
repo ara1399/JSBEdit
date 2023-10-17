fe8befe9f38d3247356ced0c414f0d9e9a6f9c7d
axb8021 <143653112+axb8021@users.noreply.github.com>
2023-10-16 21:49:08 -0500

    Add files via upload

---
+++ src/main/java/uta/cse/cse3310/JSBSimEdit/Aerodynamics.java
@@ -0,0 +1,10 @@
+package uta.cse.cse3310.JSBSimEdit;
+
+import javax.swing.*;
+
+public class Aerodynamics extends JPanel {
+    Aerodynamics(){
+        add(new JLabel("Aero"));
+        
+    }
+}
