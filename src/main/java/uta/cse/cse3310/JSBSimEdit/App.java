ce974bbbdb6f1ff582aca4ff7ee8beca36c68844
275RR <36821077+275RR@users.noreply.github.com>
2023-10-20 15:11:38 -0500

    stopped xjb generation (but saved in case needed)

--- src/main/java/uta/cse/cse3310/JSBSimEdit/App.java
+++ src/main/java/uta/cse/cse3310/JSBSimEdit/App.java
@@ -1,67 +1,26 @@
 package uta.cse.cse3310.JSBSimEdit;
 
-import java.io.File;
-import java.util.List;
-
 import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.util.SystemInfo;
-
-import jakarta.xml.bind.JAXBContext;
-import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Unmarshaller;
-import jakarta.xml.bind.Marshaller;
-import generated.FdmConfig;
 
 import javax.swing.*;
 
 public class App {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    try {
-
-      File file = new File("examples/f16.xml");
-      // JAXBContext jaxbContext = JAXBContext.newInstance(FdmConfig.class);
-
-      // Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-      // FdmConfig cfg = (FdmConfig) jaxbUnmarshaller.unmarshal(file);
-
-      // System.out.println(cfg.getName());
-
-      JAXBContext jc = JAXBContext.newInstance("generated");
-
-      Unmarshaller um = jc.createUnmarshaller();
-      FdmConfig cfg = (FdmConfig) um.unmarshal(file);
-    
-      /* System.out.println(cfg);
-      System.out.println(cfg.getFileheader().getCopyright());
-      System.out.println(cfg.getFileheader().getVersion() );
-      System.out.println(cfg.getAerodynamics().getAxis().get(0).getName());
-      System.out.println(cfg.getAerodynamics().getAxis().get(0).getFunction());
-      System.out.println(cfg.getAerodynamics().getAxis().get(0).getClass()); */
-
-
-      //Marshaller m = jc.createMarshaller();
-      //m.setProperty("jaxb.formatted.output", true);
-      //m.marshal(cfg, System.out);
-
-    } catch (JAXBException e) {
-      e.printStackTrace();
-    }
-
-    try {
-        UIManager.setLookAndFeel(new FlatDarkLaf());
-        if(SystemInfo.isLinux) {
-            // enable custom window decorations
-            JFrame.setDefaultLookAndFeelDecorated(true);
-            JDialog.setDefaultLookAndFeelDecorated(true);
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+            if(SystemInfo.isLinux) {
+                // enable custom window decorations
+                JFrame.setDefaultLookAndFeelDecorated(true);
+                JDialog.setDefaultLookAndFeelDecorated(true);
+            }
+        } catch(Exception ex) {
+            ex.printStackTrace();
         }
-    } catch(Exception ex) {
-        ex.printStackTrace();
+
+        SwingUtilities.invokeLater(() -> {
+            new MainWindow();
+        });
     }
-
-    SwingUtilities.invokeLater(() -> {
-        new MainWindow();
-    });
-
-  }
 }
