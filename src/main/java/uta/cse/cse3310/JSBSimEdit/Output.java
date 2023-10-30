8d1115a9ade6ed2c8dfce0b2878ccdeb60b1e1ec
Arham Ali <ara1399@mavs.uta.edu>
2023-10-29 20:50:14 -0500

    ch

--- src/main/java/uta/cse/cse3310/JSBSimEdit/Output.java
+++ src/main/java/uta/cse/cse3310/JSBSimEdit/Output.java
@@ -71,11 +71,12 @@ public class Output extends JPanel implements TabComponent {
 		//======== panel2 ========
 		{
 			panel2.setPreferredSize(new Dimension(745, 715));
-			panel2.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border .EmptyBorder (
-			0, 0 ,0 , 0) ,  "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn" , javax. swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder
-			. BOTTOM, new java. awt .Font ( "Dia\u006cog", java .awt . Font. BOLD ,12 ) ,java . awt. Color .
-			red ) ,panel2. getBorder () ) ); panel2. addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java .
-			beans. PropertyChangeEvent e) { if( "\u0062ord\u0065r" .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
+			panel2.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border.
+			EmptyBorder( 0, 0, 0, 0) , "JF\u006frmDes\u0069gner \u0045valua\u0074ion", javax. swing. border. TitledBorder. CENTER, javax. swing
+			. border. TitledBorder. BOTTOM, new java .awt .Font ("D\u0069alog" ,java .awt .Font .BOLD ,12 ),
+			java. awt. Color. red) ,panel2. getBorder( )) ); panel2. addPropertyChangeListener (new java. beans. PropertyChangeListener( )
+			{ @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062order" .equals (e .getPropertyName () ))
+			throw new RuntimeException( ); }} );
 			panel2.setLayout(new MigLayout(
 				"hidemode 3,align center center,gap 15 25",
 				// columns
@@ -98,6 +99,8 @@ public class Output extends JPanel implements TabComponent {
 				"[fill]" +
 				"[fill]",
 				// rows
+				"[]" +
+				"[]" +
 				"[]" +
 				"[]" +
 				"[]" +
@@ -196,7 +199,7 @@ public class Output extends JPanel implements TabComponent {
 			{
 				scrollPane1.setViewportView(textArea1);
 			}
-			panel2.add(scrollPane1, "cell 2 7 15 8,growy");
+			panel2.add(scrollPane1, "cell 2 7 13 8,growy");
 
 			//======== toolBar1 ========
 			{
@@ -216,7 +219,7 @@ public class Output extends JPanel implements TabComponent {
 				button1.setHorizontalAlignment(SwingConstants.TRAILING);
 				toolBar1.add(button1);
 			}
-			panel2.add(toolBar1, "cell 0 15 18 1,growx");
+			panel2.add(toolBar1, "cell 0 17 18 1,growx");
 		}
 		add(panel2);
 	}
