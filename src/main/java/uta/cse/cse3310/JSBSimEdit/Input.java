0d50fb29dd2ed89045f68cb157f3cff81a4bbdb8
Arham Ali <ara1399@mavs.uta.edu>
2023-10-31 22:36:45 -0500

    check

--- src/main/java/uta/cse/cse3310/JSBSimEdit/Input.java
+++ src/main/java/uta/cse/cse3310/JSBSimEdit/Input.java
@@ -1,22 +1,34 @@
 package uta.cse.cse3310.JSBSimEdit;
 
+import java.math.BigInteger;
 import java.util.Optional;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import generated.FdmConfig;
 import uta.cse.cse3310.JSBSimEdit.interfaces.TabComponent;
 
 public class Input extends JPanel implements TabComponent {
     
+JLabel portLabel;
+JTextField portText;
+
     Input() {
-        add(new JLabel("Some text"));
+        portLabel = new JLabel("Port");
+        portText = new JTextField();
     }
 
     @Override
     public void bindUIwithXML(FdmConfig cfg) {
-        // TODO
+        generated.Input in = cfg.getInput();
+        BigInteger portValue = in.getPort();
+		if (portValue != null) {
+			portText.setText(portValue.toString());
+		} else {
+			return;
+		}
     }
 
     @Override
