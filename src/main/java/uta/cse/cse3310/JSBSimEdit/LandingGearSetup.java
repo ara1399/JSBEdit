5b433848924955f7c1d30829e1123515c8eacca9
realj <realj@BlackRukh.localdomain>
2023-11-16 06:15:06 -0600

    Removed Red Text from LandingGearSetup and GroundReactions

--- src/main/java/uta/cse/cse3310/JSBSimEdit/LandingGearSetup.java
+++ src/main/java/uta/cse/cse3310/JSBSimEdit/LandingGearSetup.java
@@ -314,11 +314,6 @@ public class LandingGearSetup extends JDialog{
 
 		//======== topP ========
 		{
-		    topP.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0
-		    ,0,0,0), "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn",javax.swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM
-		    ,new java.awt.Font("Dia\u006cog",java.awt.Font.BOLD,12),java.awt.Color.red),
-		    topP. getBorder()));topP. addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e
-		    ){if("\u0062ord\u0065r".equals(e.getPropertyName()))throw new RuntimeException();}});
 		    topP.setLayout(new MigLayout(
 			"hidemode 3,alignx center",
 			// columns
