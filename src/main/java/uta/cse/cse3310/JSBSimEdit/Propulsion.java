a5c8a1b0363541c1efd018dffd33e2fe3e1f323a
Arham Ali <ara1399@mavs.uta.edu>
2023-11-16 20:56:14 -0600

    pp

--- src/main/java/uta/cse/cse3310/JSBSimEdit/Propulsion.java
+++ src/main/java/uta/cse/cse3310/JSBSimEdit/Propulsion.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
